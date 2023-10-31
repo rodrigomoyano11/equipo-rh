f86c670b381b72ce7210a94e19c066fa926fa023
Rodrigo Moyano <rodrigomoyano11@gmail.com>
2023-10-30 22:28:00 -0300

    🧠 Configure "i18nProvider" for "react-aria" in the "Providers" component

--- src/app/_components/Providers/ClientProviders/component.tsx
+++ src/app/_components/Providers/ClientProviders/component.tsx
@@ -2,9 +2,12 @@
 
 import { ThemeProvider } from '../../ThemeProvider'
 import { ClientProvidersProps } from './types'
+import { I18nProvider as AriaI18nProvider } from '@react-aria/i18n'
 
 const ClientProviders = ({ children }: ClientProvidersProps) => (
-  <ThemeProvider>{children}</ThemeProvider>
+  <ThemeProvider>
+    <AriaI18nProvider locale="es">{children}</AriaI18nProvider>
+  </ThemeProvider>
 )
 
 export { ClientProviders }
