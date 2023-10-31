08c1a18571be89347192b450fe17a8c9334c6302
Rodrigo Moyano <rodrigomoyano11@gmail.com>
2023-10-31 00:10:33 -0300

    🧠 Implement "react-aria" in the "TextField" component

--- src/app/_components/TextField/component.tsx
+++ src/app/_components/TextField/component.tsx
@@ -1,20 +1,73 @@
+'use client'
+
+import { FieldComponent } from '@/types/forms'
 import { useId } from 'react'
+import {
+  Input as AriaInput,
+  Label as AriaLabel,
+  Text as AriaText,
+  TextField as AriaTextField,
+  TextFieldProps as AriaTextFieldProps,
+  TextArea as AriaTextArea,
+} from 'react-aria-components'
+import { useController } from 'react-hook-form'
+import './styles.css'
 import { TextFieldProps } from './types'
 
-const TextField = ({ defaultValue, name, autoComplete, label }: TextFieldProps) => {
+const TextField: FieldComponent<TextFieldProps> = (props) => {
+  // Props
+  const { name, label, description, placeholder, largeText, isDisabled, ...restOfProps } = props
+
+  // Hooks
   const id = useId()
 
+  const { field, fieldState } = useController({ name, disabled: isDisabled })
+
+  // Data
+  const { value, disabled, onBlur, onChange, ref } = field
+
+  const errorMessage = fieldState.error?.message
+
+  // Render
+  const ariaLabelProps = label ? { 'aria-labelledby': id } : { id, 'aria-label': name }
+
+  const ariaTextFieldProps: AriaTextFieldProps = {
+    isDisabled: disabled,
+    value: value ?? '',
+    onBlur,
+    onChange,
+    isInvalid: Boolean(errorMessage),
+    name,
+    ...restOfProps,
+    ...ariaLabelProps,
+  }
+
   return (
-    <>
-      <label htmlFor={id}>{label}</label>
-      <input
-        autoComplete={autoComplete}
-        defaultValue={defaultValue}
-        id={id}
-        name={name}
-        type="text"
-      />
-    </>
+    <AriaTextField ref={ref} {...ariaTextFieldProps} className="text-field">
+      {label && (
+        <AriaLabel className="label" htmlFor={id}>
+          {label}
+        </AriaLabel>
+      )}
+
+      {largeText ? (
+        <AriaTextArea className="large-input" id={id} placeholder={placeholder} />
+      ) : (
+        <AriaInput className="default-input" id={id} placeholder={placeholder} />
+      )}
+
+      {!errorMessage && description && (
+        <AriaText className="description" slot="description">
+          {description}
+        </AriaText>
+      )}
+
+      {errorMessage && (
+        <AriaText className="error-message" slot="errorMessage">
+          {errorMessage}
+        </AriaText>
+      )}
+    </AriaTextField>
   )
 }
 
