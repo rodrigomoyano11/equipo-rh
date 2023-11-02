7ccfe8484684ad0b17882f7e82cba11364bfde31
Rodrigo Moyano <rodrigomoyano11@gmail.com>
2023-11-02 00:55:26 -0300

    🧠 Add compatibility with "react-hook-form" to "NumberField" component

--- src/app/_components/NumberField/component.tsx
+++ src/app/_components/NumberField/component.tsx
@@ -1,14 +1,80 @@
+'use client'
+
+import { FieldComponent } from '@/types/forms'
 import { useId } from 'react'
+import {
+  Button as AriaButton,
+  Group as AriaGroup,
+  Input as AriaInput,
+  Label as AriaLabel,
+  NumberField as AriaNumberField,
+  NumberFieldProps as AriaNumberFieldProps,
+  Text as AriaText,
+} from 'react-aria-components'
+import { useController } from 'react-hook-form'
+import './styles.css'
 import { NumberFieldProps } from './types'
 
-const NumberField = ({ defaultValue, name, label }: NumberFieldProps) => {
+const NumberField: FieldComponent<NumberFieldProps> = (props) => {
+  // Props
+  const { name, label, description, placeholder, isDisabled, className, ...restOfProps } = props
+
+  // Hooks
   const id = useId()
 
+  const { field, fieldState } = useController({ name, disabled: isDisabled })
+
+  // Data
+  const { value, disabled, onBlur, onChange, ref } = field
+
+  const errorMessage = fieldState.error?.message
+
+  // Render
+  const ariaLabelProps = label ? { 'aria-labelledby': id } : { id, 'aria-label': name }
+
+  const ariaNumberFieldProps: AriaNumberFieldProps = {
+    isDisabled: disabled,
+    value: value ?? undefined,
+    onBlur,
+    onChange,
+    isInvalid: Boolean(errorMessage),
+    name,
+    ...restOfProps,
+    ...ariaLabelProps,
+  }
+
   return (
-    <>
-      <label htmlFor={id}>{label}</label>
-      <input defaultValue={defaultValue} id={id} name={name} type="number" />
-    </>
+    <AriaNumberField ref={ref} className={`number-field ${className}`} {...ariaNumberFieldProps}>
+      {label && (
+        <AriaLabel className="label" htmlFor={id}>
+          {label}
+        </AriaLabel>
+      )}
+
+      <AriaGroup className="input-group">
+        <AriaButton className="decrement-button" slot="decrement">
+          -
+        </AriaButton>
+
+        <AriaInput className="input" placeholder={placeholder} />
+
+        <AriaButton className="increment-button" slot="increment">
+          +
+        </AriaButton>
+      </AriaGroup>
+
+      {!errorMessage && description && (
+        <AriaText className="description" slot="description">
+          {description}
+        </AriaText>
+      )}
+
+      {errorMessage && (
+        <AriaText className="error-message" slot="errorMessage">
+          {errorMessage}
+        </AriaText>
+      )}
+    </AriaNumberField>
   )
 }
 
--- src/app/_components/NumberField/types.ts
+++ src/app/_components/NumberField/types.ts
@@ -1,7 +1,8 @@
-type NumberFieldProps = {
-  label: string
-  defaultValue?: string
-  name: string
-}
+import { FieldProps } from '@/types/forms'
+import { NumberFieldProps as AriaNumberFieldProps } from 'react-aria-components'
+
+type SelectedProps = Pick<AriaNumberFieldProps, 'onCopy' | 'onCut' | 'onPaste' | 'onSelect'>
+
+type NumberFieldProps = FieldProps & SelectedProps & { placeholder?: string }
 
 export type { NumberFieldProps }
