0b97855a8c5d6ab19ff8ca17c8ef33d65caf0ab8
Rodrigo Moyano <rodrigomoyano11@gmail.com>
2023-11-03 08:43:03 -0300

    🔥 Delete types of "jobs" page

--- src/app/(pages)/(platform)/jobs/types.ts
+++
@@ -1,9 +0,0 @@
-import { Tables } from '@/utils/supabase/types'
-
-type AddJobRequest = Omit<Tables<'jobs'>, 'created_at' | 'id'>
-
-type AddJobResponse = AddJobRequest
-
-type AddJobAction = (_?: AddJobResponse, formData?: FormData) => Promise<AddJobResponse | undefined>
-
-export type { AddJobAction, AddJobRequest, AddJobResponse }
